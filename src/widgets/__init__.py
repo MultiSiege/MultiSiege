9fcc7d8be13b8dfd0bab951e04e82153fdd9d90b
Ernie <erniedatryhard@gmail.com>
2024-04-27 19:57:29 +0100

    Resorted file structure to be more organised.

--- src/widgets/__init__.py
+++ src/widgets/__init__.py
@@ -1,6 +1,6 @@
-from .main_window import Ui_MainWindow
+from .Main.main import MainWindow
 
 #explicitly declare the outwards facing API of this module
 __all__ = [
-    "Ui_MainWindow"
+    "MainWindow"
 ]
