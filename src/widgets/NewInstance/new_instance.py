b4e4d0424c10ab76bf29af6e63fe560aa590e89c
Ernie <erniedatryhard@gmail.com>
2024-04-28 19:36:12 +0100

    Added new custom class for seasons model

--- src/widgets/NewInstance/new_instance.py
+++ src/widgets/NewInstance/new_instance.py
@@ -6,21 +6,26 @@ from PySide6 import (
 )
 from UI.new_instance_window import Ui_NewInstance
 
+class SeasonsModel(qtg.QStandardItemModel):
+    def __init__(self):
+        super().__init__()
+
+        self.setColumnCount(4)
+        self.setHorizontalHeaderLabels(["Name", "Version", "Released", "Liberator Supported"])
+
+        for i in range(0, 4):
+            item = qtg.QStandardItem()
+            item.setText("hello")
+            self.setItem(0, i, item)
+
 class NewInstance(qtw.QDialog, Ui_NewInstance):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
 
-        model = qtg.QStandardItemModel()
-        model.setColumnCount(4)
-        model.setVerticalHeaderLabels(["Name", "Version", "Released", "Liberator Supported"])
-        parentItem = model.invisibleRootItem()
-        for i in range(0, 4):
-            item = qtg.QStandardItem()
-            item.setText("hello")
-            parentItem.appendColumn([item, item, item, item])
+        self.model = SeasonsModel()
 
-        self.treeView_seasons.setModel(model)
+        self.treeView_seasons.setModel(self.model)
 
         self.filter_liberator.clicked.connect(self.apply_liberator_filter)
 
