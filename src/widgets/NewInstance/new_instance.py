8955b4b153c8bd25e80b7093464a9ef640c5dcff
Ernie <erniedatryhard@gmail.com>
2024-04-29 19:30:24 +0100

    Updated new_instance.py so that the model is working (time for filters)

--- src/widgets/NewInstance/new_instance.py
+++ src/widgets/NewInstance/new_instance.py
@@ -6,26 +6,49 @@ from PySide6 import (
 )
 from UI.new_instance_window import Ui_NewInstance
 
+sys.path.append('src') #python can't usually find the constants module, so we have to manually append it to our path
+from constants import *
+
 class SeasonsModel(qtg.QStandardItemModel):
     def __init__(self):
         super().__init__()
 
-        self.setColumnCount(4)
-        self.setHorizontalHeaderLabels(["Name", "Version", "Released", "Liberator Supported"])
+        self.setColumnCount(3)
+        self.setHorizontalHeaderLabels(["Name", "Version", "Liberator Supported"])
 
-        for i in range(0, 4):
-            item = qtg.QStandardItem()
-            item.setText("hello")
-            self.setItem(0, i, item)
+        for row, version in enumerate(list(SiegeVersions)):
+            for column in range(3):
+                item = qtg.QStandardItem()
+
+                if column == 0:
+                    item.setText(self.format_season_label(version.name))
+                elif column == 1:
+                    item.setText(version.value)
+                elif column == 2:
+                    if int(version.value[1]) < 5: #arbitrary values that are gonna be used once, no need for constants (unless liberator gets updated, which is never happening)
+                        item.setText("Yes")
+                    else:
+                        item.setText("No")
+                
+                self.setItem(row, column, item)
+
+    def format_season_label(self, label: str) -> str:
+        label_list = label.split("_")
+
+        new_label = ""
+
+        for label in label_list:
+            new_label += label[0].upper() + label[1:].lower() + " "
+
+        return new_label[:-1]
 
 class NewInstance(qtw.QDialog, Ui_NewInstance):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
 
-        self.model = SeasonsModel()
-
-        self.treeView_seasons.setModel(self.model)
+        self.base_model = SeasonsModel()
+        self.treeView_seasons.setModel(self.base_model)
 
         self.filter_liberator.clicked.connect(self.apply_liberator_filter)
 
