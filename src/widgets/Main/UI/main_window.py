38c1097cba26001def82973e75c61b6809772784
Ernie <erniedatryhard@gmail.com>
2024-06-04 20:58:33 +0100

    updated main window UI so it is less jank

--- src/widgets/Main/UI/main_window.py
+++ src/widgets/Main/UI/main_window.py
@@ -15,31 +15,40 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QFrame, QGridLayout, QHBoxLayout,
-    QLabel, QPushButton, QScrollArea, QSizePolicy,
-    QSpacerItem, QVBoxLayout, QWidget)
+from PySide6.QtWidgets import (QApplication, QFrame, QGridLayout, QLabel,
+    QPushButton, QScrollArea, QSizePolicy, QSpacerItem,
+    QVBoxLayout, QWidget)
 from . import icons
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(639, 557)
+        MainWindow.resize(748, 557)
         icon = QIcon()
         icon.addFile(u":/main_widget_logo/logo-TB.png", QSize(), QIcon.Normal, QIcon.Off)
         MainWindow.setWindowIcon(icon)
         self.gridLayout = QGridLayout(MainWindow)
         self.gridLayout.setObjectName(u"gridLayout")
-        self.verticalSpacer = QSpacerItem(20, 40, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.MinimumExpanding)
+        self.horizontalSpacer = QSpacerItem(40, 20, QSizePolicy.Policy.MinimumExpanding, QSizePolicy.Policy.Minimum)
 
-        self.gridLayout.addItem(self.verticalSpacer, 3, 2, 1, 1)
+        self.gridLayout.addItem(self.horizontalSpacer, 0, 6, 1, 1)
 
-        self.widget_topnav = QWidget(MainWindow)
-        self.widget_topnav.setObjectName(u"widget_topnav")
-        self.widget_topnav.setMaximumSize(QSize(16777215, 68))
-        self.horizontalLayout = QHBoxLayout(self.widget_topnav)
-        self.horizontalLayout.setObjectName(u"horizontalLayout")
-        self.pb_add_instance = QPushButton(self.widget_topnav)
+        self.line_5 = QFrame(MainWindow)
+        self.line_5.setObjectName(u"line_5")
+        self.line_5.setFrameShape(QFrame.Shape.VLine)
+        self.line_5.setFrameShadow(QFrame.Shadow.Sunken)
+
+        self.gridLayout.addWidget(self.line_5, 0, 5, 1, 1)
+
+        self.line_4 = QFrame(MainWindow)
+        self.line_4.setObjectName(u"line_4")
+        self.line_4.setFrameShape(QFrame.Shape.VLine)
+        self.line_4.setFrameShadow(QFrame.Shadow.Sunken)
+
+        self.gridLayout.addWidget(self.line_4, 0, 1, 1, 1)
+
+        self.pb_add_instance = QPushButton(MainWindow)
         self.pb_add_instance.setObjectName(u"pb_add_instance")
         self.pb_add_instance.setMinimumSize(QSize(115, 50))
         icon1 = QIcon()
@@ -47,68 +56,17 @@ class Ui_MainWindow(object):
         self.pb_add_instance.setIcon(icon1)
         self.pb_add_instance.setIconSize(QSize(20, 20))
 
-        self.horizontalLayout.addWidget(self.pb_add_instance)
+        self.gridLayout.addWidget(self.pb_add_instance, 0, 0, 1, 1)
 
-        self.line_4 = QFrame(self.widget_topnav)
-        self.line_4.setObjectName(u"line_4")
-        self.line_4.setFrameShape(QFrame.Shape.VLine)
-        self.line_4.setFrameShadow(QFrame.Shadow.Sunken)
-
-        self.horizontalLayout.addWidget(self.line_4)
-
-        self.pb_folders = QPushButton(self.widget_topnav)
-        self.pb_folders.setObjectName(u"pb_folders")
-        self.pb_folders.setMinimumSize(QSize(100, 50))
-        icon2 = QIcon()
-        icon2.addFile(u":/top_nav_bar/285658_blue_folder_icon.svg", QSize(), QIcon.Normal, QIcon.Off)
-        self.pb_folders.setIcon(icon2)
-        self.pb_folders.setIconSize(QSize(20, 20))
-
-        self.horizontalLayout.addWidget(self.pb_folders)
-
-        self.pb_settings = QPushButton(self.widget_topnav)
+        self.pb_settings = QPushButton(MainWindow)
         self.pb_settings.setObjectName(u"pb_settings")
         self.pb_settings.setMinimumSize(QSize(100, 50))
-        icon3 = QIcon()
-        icon3.addFile(u":/top_nav_bar/settings-cog.svg", QSize(), QIcon.Normal, QIcon.Off)
-        self.pb_settings.setIcon(icon3)
+        icon2 = QIcon()
+        icon2.addFile(u":/top_nav_bar/settings-cog.svg", QSize(), QIcon.Normal, QIcon.Off)
+        self.pb_settings.setIcon(icon2)
         self.pb_settings.setIconSize(QSize(20, 20))
 
-        self.horizontalLayout.addWidget(self.pb_settings)
-
-        self.pb_help = QPushButton(self.widget_topnav)
-        self.pb_help.setObjectName(u"pb_help")
-        self.pb_help.setMinimumSize(QSize(100, 50))
-        icon4 = QIcon()
-        icon4.addFile(u":/top_nav_bar/Blue_Question_Mark_clip_art.svg", QSize(), QIcon.Normal, QIcon.Off)
-        self.pb_help.setIcon(icon4)
-        self.pb_help.setIconSize(QSize(20, 20))
-
-        self.horizontalLayout.addWidget(self.pb_help)
-
-        self.line_5 = QFrame(self.widget_topnav)
-        self.line_5.setObjectName(u"line_5")
-        self.line_5.setFrameShape(QFrame.Shape.VLine)
-        self.line_5.setFrameShadow(QFrame.Shadow.Sunken)
-
-        self.horizontalLayout.addWidget(self.line_5)
-
-        self.horizontalSpacer = QSpacerItem(40, 20, QSizePolicy.Policy.MinimumExpanding, QSizePolicy.Policy.Minimum)
-
-        self.horizontalLayout.addItem(self.horizontalSpacer)
-
-        self.pb_accounts = QPushButton(self.widget_topnav)
-        self.pb_accounts.setObjectName(u"pb_accounts")
-        self.pb_accounts.setMinimumSize(QSize(100, 50))
-        icon5 = QIcon()
-        icon5.addFile(u":/top_nav_bar/Steam_icon_logo.svg", QSize(), QIcon.Normal, QIcon.Off)
-        self.pb_accounts.setIcon(icon5)
-        self.pb_accounts.setIconSize(QSize(20, 20))
-
-        self.horizontalLayout.addWidget(self.pb_accounts)
-
-
-        self.gridLayout.addWidget(self.widget_topnav, 0, 0, 1, 3)
+        self.gridLayout.addWidget(self.pb_settings, 0, 3, 1, 1)
 
         self.widget_sidebar = QWidget(MainWindow)
         self.widget_sidebar.setObjectName(u"widget_sidebar")
@@ -193,27 +151,51 @@ class Ui_MainWindow(object):
         self.verticalLayout.addWidget(self.line_6)
 
 
-        self.gridLayout.addWidget(self.widget_sidebar, 1, 2, 2, 1)
+        self.gridLayout.addWidget(self.widget_sidebar, 1, 9, 2, 1)
 
-        self.frame_instances = QFrame(MainWindow)
-        self.frame_instances.setObjectName(u"frame_instances")
-        self.frame_instances.setMinimumSize(QSize(0, 0))
-        self.frame_instances.setFrameShape(QFrame.Shape.StyledPanel)
-        self.frame_instances.setFrameShadow(QFrame.Shadow.Raised)
-        self.horizontalLayout_2 = QHBoxLayout(self.frame_instances)
-        self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
-        self.scrollArea = QScrollArea(self.frame_instances)
+        self.pb_help = QPushButton(MainWindow)
+        self.pb_help.setObjectName(u"pb_help")
+        self.pb_help.setMinimumSize(QSize(100, 50))
+        icon3 = QIcon()
+        icon3.addFile(u":/top_nav_bar/Blue_Question_Mark_clip_art.svg", QSize(), QIcon.Normal, QIcon.Off)
+        self.pb_help.setIcon(icon3)
+        self.pb_help.setIconSize(QSize(20, 20))
+
+        self.gridLayout.addWidget(self.pb_help, 0, 4, 1, 1)
+
+        self.scrollArea = QScrollArea(MainWindow)
         self.scrollArea.setObjectName(u"scrollArea")
         self.scrollArea.setWidgetResizable(True)
         self.scrollAreaWidgetContents = QWidget()
         self.scrollAreaWidgetContents.setObjectName(u"scrollAreaWidgetContents")
-        self.scrollAreaWidgetContents.setGeometry(QRect(0, 0, 478, 443))
+        self.scrollAreaWidgetContents.setGeometry(QRect(0, 0, 607, 481))
         self.scrollArea.setWidget(self.scrollAreaWidgetContents)
 
-        self.horizontalLayout_2.addWidget(self.scrollArea)
+        self.gridLayout.addWidget(self.scrollArea, 1, 0, 3, 9)
 
+        self.verticalSpacer = QSpacerItem(20, 40, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.MinimumExpanding)
 
-        self.gridLayout.addWidget(self.frame_instances, 1, 0, 3, 2)
+        self.gridLayout.addItem(self.verticalSpacer, 3, 9, 1, 1)
+
+        self.pb_folders = QPushButton(MainWindow)
+        self.pb_folders.setObjectName(u"pb_folders")
+        self.pb_folders.setMinimumSize(QSize(100, 50))
+        icon4 = QIcon()
+        icon4.addFile(u":/top_nav_bar/285658_blue_folder_icon.svg", QSize(), QIcon.Normal, QIcon.Off)
+        self.pb_folders.setIcon(icon4)
+        self.pb_folders.setIconSize(QSize(20, 20))
+
+        self.gridLayout.addWidget(self.pb_folders, 0, 2, 1, 1)
+
+        self.pb_accounts = QPushButton(MainWindow)
+        self.pb_accounts.setObjectName(u"pb_accounts")
+        self.pb_accounts.setMinimumSize(QSize(100, 50))
+        icon5 = QIcon()
+        icon5.addFile(u":/top_nav_bar/Steam_icon_logo.svg", QSize(), QIcon.Normal, QIcon.Off)
+        self.pb_accounts.setIcon(icon5)
+        self.pb_accounts.setIconSize(QSize(20, 20))
+
+        self.gridLayout.addWidget(self.pb_accounts, 0, 9, 1, 1)
 
 
         self.retranslateUi(MainWindow)
@@ -224,10 +206,7 @@ class Ui_MainWindow(object):
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MultiSiege - Main Window", None))
         self.pb_add_instance.setText(QCoreApplication.translate("MainWindow", u"Add instance", None))
-        self.pb_folders.setText(QCoreApplication.translate("MainWindow", u"Folders", None))
         self.pb_settings.setText(QCoreApplication.translate("MainWindow", u"Settings", None))
-        self.pb_help.setText(QCoreApplication.translate("MainWindow", u"Help", None))
-        self.pb_accounts.setText(QCoreApplication.translate("MainWindow", u"Accounts", None))
         self.label_instance_icon_sidebar.setText("")
         self.label_instance_name.setText(QCoreApplication.translate("MainWindow", u"INSTANCE_NAME", None))
         self.pb_launch.setText(QCoreApplication.translate("MainWindow", u"Launch", None))
@@ -237,5 +216,8 @@ class Ui_MainWindow(object):
         self.pb_siege_folder.setText(QCoreApplication.translate("MainWindow", u"Siege Folder", None))
         self.pb_create_shortcut.setText(QCoreApplication.translate("MainWindow", u"Create Shortcut", None))
         self.pb_delete.setText(QCoreApplication.translate("MainWindow", u"Delete", None))
+        self.pb_help.setText(QCoreApplication.translate("MainWindow", u"Help", None))
+        self.pb_folders.setText(QCoreApplication.translate("MainWindow", u"Folders", None))
+        self.pb_accounts.setText(QCoreApplication.translate("MainWindow", u"Accounts", None))
     # retranslateUi
 
